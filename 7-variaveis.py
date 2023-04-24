3a6ddbe19032de77fdb34671d4df2adf5530ad3a
Josiane Rocha <josi1992@gmail.com>
2023-04-23 22:03:04 -0300

    variáveis no python

---
+++ 7-variaveis.py
@@ -0,0 +1,13 @@
+# Variáveis são usadas para salvar algo na memória do computador
+# PEP8: inicie variáveis com letras  minúsculas, pode usar
+# números e underline _.
+# O sianl de = é um operador de atribuição. Ele é usado para 
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+nome = 'Josi'
+idade = 30
+maior_de_idade = idade >=18
+
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
