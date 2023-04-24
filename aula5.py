28f6ff0afb834aaeb7078ba8767cf0af0ced9b1c
Josiane Rocha <josi1992@gmail.com>
2023-04-23 21:09:46 -0300

    tipos de dados: bool

--- aula5.py
+++ aula5.py
@@ -1,5 +1,12 @@
-#tipos de dados bool (boolean)
+# tipos de dados bool (boolean)
 # Ao questionar algo em um programa,
 # só existe duas respostas possiveis:
-# sim(true) ou não(false).
-#Existem vários operadores para "questionar"
+# sim(True) ou não(False).
+# Existem vários operadores para "questionar"
+# dentre eles o ==, que e um operador logico que
+# questiona se um valor é igual ao outro.
+print(10 == 10) # sim => True (Verdadeiro)
+print(10 == 11) # não => False (Falso)
+print(type(10 == 10))
+print(type(10 == 11))
+
