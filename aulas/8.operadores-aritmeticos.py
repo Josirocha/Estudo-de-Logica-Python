e2b3300d83f93bb5fe84dd799cb8dffe7342022a
Josiane Rocha <josi1992@gmail.com>
2023-04-23 22:54:05 -0300

    operadores aritmeticos

---
+++ aulas/8.operadores-aritmeticos.py
@@ -0,0 +1,27 @@
+adicao = 10 + 10
+print('Adiçao', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2  #float
+print('Multiplicação', multiplicacao)
+
+divisao_inteira = 10 // 2.2 # corta as casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 25 % 5 # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 2 == 0) # par
+print(16 % 2 == 0) # par
+print(15 % 2 == 0) # impar
+
+
+
