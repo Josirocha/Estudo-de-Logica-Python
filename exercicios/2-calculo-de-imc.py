3a399c2708ddc940ce534b7c292b50003526e710
Josiane Rocha <josi1992@gmail.com>
2023-04-27 19:47:35 -0300

    feat: exercicío de calculo de imc

---
+++ exercicios/2-calculo-de-imc.py
@@ -0,0 +1,7 @@
+nome = 'Josiane'
+altura = 1.62
+peso = 64
+imc =  peso / (altura * altura)
+# imc =  peso / altura ** 2
+print (nome,'tem', altura,'de altura,',)
+print('pesa', peso, 'quilos e seu IMC é', imc)
