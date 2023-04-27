ccfd1da78188776c2351827267d7340bc99b78a1
Josiane Rocha <josi1992@gmail.com>
2023-04-27 19:53:52 -0300

    feat: introducao as condicionais

---
+++ aulas/14.condicionais(introducoes).py
@@ -0,0 +1,10 @@
+# if / elif    / else
+# se / se nã se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você mão digitou nem entrar e nem sair.')
