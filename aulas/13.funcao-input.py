fd35a8d21de6bb9f58076179e0b846b2ded76898
Josiane Rocha <josi1992@gmail.com>
2023-04-27 19:52:15 -0300

    feat: função input

---
+++ aulas/13.funcao-input.py
@@ -0,0 +1,7 @@
+# nome = input('qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+print(f'A soma dos numeros é: {int(numero_1) + int(numero_2)}')
