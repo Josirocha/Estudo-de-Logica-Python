46a1257938965f6a2eda13e57e848904d3019442
Josiane Rocha <josi1992@gmail.com>
2023-05-18 08:15:50 -0300

    feat: operador logico not

---
+++ aulas/19.operador-logico-not.py
@@ -0,0 +1,12 @@
+# Operador lógico "not"
+# Usado para inverter expressões 
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if not senha:
+#     print('Você não digitou')
+
+print(not True) #False
+print(not False) #True
